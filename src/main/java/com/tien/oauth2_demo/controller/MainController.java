6ae749bfd0abdd52d8743ca46ff69e8c0bc2aa20
nqtienvn <quangtiencoder1876@gmail.com>
2025-09-28 15:54:42 +0700

    add origin

---
+++ src/main/java/com/tien/oauth2_demo/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.tien.oauth2_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "main.html";
+    }
+}
