f1494805ce478c2a904d8e1ed25c4b49e8b79b0f
nqtienvn <quangtiencoder1876@gmail.com>
2025-09-29 17:06:27 +0700

    nothing

---
+++ src/main/java/com/tien/oauth2_demo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.tien.oauth2_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeController {
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello!";
+        }
+}
