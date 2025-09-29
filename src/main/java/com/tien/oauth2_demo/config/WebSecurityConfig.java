025ad19fa79051d007fe9b242bea3a9b3c396ab3
nqtienvn <quangtiencoder1876@gmail.com>
2025-09-29 15:08:44 +0700

    not

---
+++ src/main/java/com/tien/oauth2_demo/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.tien.oauth2_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("tien1")
+                .password("123")
+                .authorities("read") //permission
+                .build();
+        userDetailService.createUser(user);
+        return userDetailService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
